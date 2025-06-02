618f9449e50ee97550fb3812554cf80414282c71
Farid <tavakoli127@gmail.com>
2025-06-02 13:31:10 +0330

    adding UserModel and UserError basic

---
+++ src/Tasker.Domain/User/UserError.cs
@@ -0,0 +1,15 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasker.Domain.User
+{
+    public class UserError
+    {
+        public Error UserNotFound = Error.NotFound
+            (code: "user.not.found" , description: "User Not Found");
+    }
+}
---
+++ src/Tasker.Domain/User/UserModel.cs
@@ -0,0 +1,54 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasker.Domain.Common;
+
+namespace Tasker.Domain.User
+{
+    public class UserModel : BaseModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool isDelete{ get; set; }
+
+        //navigation
+
+
+        //ctor
+        public UserModel(
+            string _name,
+            string _email,
+            string _password)
+        {
+            Id = Guid.NewGuid();
+            Name = _name;
+            Email = _email;
+            Password = _password;
+        }
+
+        //methods
+        public void SetName(string value)
+        {
+            Name = value;
+        }
+        public void SetEmail(string value)
+        {
+            Email = value;
+        }
+        public void SetPassword(string value)
+        {
+            Password = value;
+        }
+        public void Delete()
+        {
+            isDelete = !isDelete;
+        }
+    }
+}
