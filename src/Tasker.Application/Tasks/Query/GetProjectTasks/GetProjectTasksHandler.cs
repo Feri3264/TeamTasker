df6ebe83c37bd0587f7fb671f6f6d940df404dc5
Farid <tavakoli127@gmail.com>
2025-06-04 16:50:53 +0330

    adding GetProjectTasks Query and Query handler in application

---
+++ src/Tasker.Application/Tasks/Query/GetProjectTasks/GetProjectTasksHandler.cs
@@ -0,0 +1,27 @@
+﻿using ErrorOr;
+using MediatR;
+using Tasker.Application.Common.Interfaces.Repositories;
+using Tasker.Domain.Project;
+using Tasker.Domain.Tasks;
+
+namespace Tasker.Application.Tasks.Query.GetProjectTasks;
+
+public class GetProjectTasksHandler
+    (ITaskRepository taskRepository,
+        IProjectRepository projectRepository) : IRequestHandler<GetProjectTasksQuery , ErrorOr<List<TaskModel>>>
+{
+    public async Task<ErrorOr<List<TaskModel>>> Handle(GetProjectTasksQuery request, CancellationToken cancellationToken)
+    {
+        var project = await projectRepository.GetByIdAsync(request.projectId);
+
+        if (project is null)
+            return ProjectError.ProjectNotFound;
+
+        var tasks = await taskRepository.GetByIdsAsync(project.TaskIds);
+
+        if (tasks is null)
+            return TaskError.TaskNotFound;
+
+        return tasks;
+    }
+}
---
+++ src/Tasker.Application/Tasks/Query/GetProjectTasks/GetProjectTasksQuery.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+using MediatR;
+using Tasker.Domain.Tasks;
+
+namespace Tasker.Application.Tasks.Query.GetProjectTasks;
+
+public record GetProjectTasksQuery(Guid projectId) : IRequest<ErrorOr<List<TaskModel>>>;
