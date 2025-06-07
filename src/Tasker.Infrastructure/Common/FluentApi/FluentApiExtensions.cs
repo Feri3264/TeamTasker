60a8880539d3cba7493b3ee1a2447df829e8b808
Farid <tavakoli127@gmail.com>
2025-06-07 11:40:40 +0330

    adding FluentApiExtensions and ListOfIdsConverter to Infrastructure

---
+++ src/Tasker.Infrastructure/Common/FluentApi/FluentApiExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Tasker.Infrastructure.Common.FluentApi;
+
+public static class FluentApiExtensions
+{
+    public static PropertyBuilder<T> HasListOfIdsConverter<T>(this PropertyBuilder<T> builder)
+    {
+        return builder.HasConversion(
+            new ListOfIdsConverter());
+    }
+}
---
+++ src/Tasker.Infrastructure/Common/FluentApi/ListOfIdsConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Tasker.Infrastructure.Common.FluentApi;
+
+public class ListOfIdsConverter : ValueConverter<List<Guid>, string>
+{
+    public ListOfIdsConverter(ConverterMappingHints? mappingHints = null)
+        : base(
+            c => string.Join(',', c),
+            c => c.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(Guid.Parse).ToList(),
+            mappingHints
+        )
+    { }
+}
