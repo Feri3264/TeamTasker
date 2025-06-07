b246d631d58b87ff739c698188fcd28704d2901f
Farid <tavakoli127@gmail.com>
2025-06-07 12:34:33 +0330

    adding TaskConfig to Infrastructure

---
+++ src/Tasker.Infrastructure/Task/TaskConfig.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tasker.Domain.Project;
+using Tasker.Domain.Tasks;
+using Tasker.Domain.User;
+
+namespace Tasker.Infrastructure.Task;
+
+public class TaskConfig : IEntityTypeConfiguration<TaskModel>
+{
+    public void Configure(EntityTypeBuilder<TaskModel> builder)
+    {
+        //properties
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Id).ValueGeneratedNever();
+
+        builder.Property(t => t.Name).HasMaxLength(100).IsRequired();
+        builder.Property(t => t.Status).IsRequired();
+        builder.Property(t => t.Priority).IsRequired();
+        builder.Property(t => t.AssignedMemberId).ValueGeneratedNever().IsRequired();
+        builder.Property(t => t.ProjectId).ValueGeneratedNever().IsRequired();
+
+        builder.Property(t => t.Deadline).HasColumnType("time without time zone")
+            .IsRequired();
+
+
+        //navigation
+        builder.HasOne<UserModel>()
+            .WithMany()
+            .HasForeignKey(t => t.AssignedMemberId);
+
+        builder.HasOne<ProjectModel>()
+            .WithMany()
+            .HasForeignKey(t => t.ProjectId);
+
+    }
+}
--- src/Tasker.Infrastructure/obj/Debug/net9.0/Tasker.Infrastructure.AssemblyInfo.cs
+++ src/Tasker.Infrastructure/obj/Debug/net9.0/Tasker.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasker.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0bad29ca2187be7139ee008a5eead454cab75b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10058fcb1998f39905676b227c749a4375e6951")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasker.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasker.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
